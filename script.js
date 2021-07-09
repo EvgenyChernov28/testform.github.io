c95f7fca4315d3ffcceb2d662a86de96d55220ab
EvgenyChernov28 <78750169+EvgenyChernov28@users.noreply.github.com>
2021-07-09 14:56:01 +0300

    v.01 - add more product

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 
-let a = document.getElementById('products')
+let products = document.getElementById('products')
 
 let cardConteiner = document.getElementById("card-conteiner")
 
 let cardProducts = document.getElementById("card-products")
-console.log(cardConteiner);
 
-a.addEventListener('click', () =>(
+products.addEventListener('click', () =>(
     cardConteiner.classList.add('card-none'),
     cardProducts.classList.remove('card-none')
+))
+
+let continueButton = document.getElementById("continue-button")
+
+let choseProducts = document.getElementById("chose-products")
+
+console.log(continueButton);
+
+continueButton.addEventListener('click', () =>(
+    cardProducts.classList.add('card-none'),
+    choseProducts.classList.remove('card-none')
 ))
